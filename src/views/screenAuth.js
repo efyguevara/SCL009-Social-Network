6a0cce04457e2db31d593b229e4c0737219038f7
MarinesLemus007 <marineslemus@gmail.com>
2019-06-05 14:04:57 -0400

    modificando el test, return true a validateLogin

--- src/views/screenAuth.js
+++ src/views/screenAuth.js
@@ -12,19 +12,19 @@ export const screenAuth = () => {
     <h2>Iniciar Sesión</h2>
     <input type="email" id="email" placeholder="&#128272; Correo" name="email" required>
     <input type="password" id="password" placeholder="&#128272; Contraseña" name="password" required>
-    <button value="Entrar">Entrar</button>
-    <button id="buttonGoogle" value="Entrar con Google">Entrar con Google</button>
-    <button id="btn-checkin" value="Registrarse">Registrarse</button>
+    <button type="button" value="Entrar">Entrar</button>
+    <button type="button" id="buttonGoogle" value="Entrar con Google">Entrar con Google</button>
+    <button type="button" id="btn-checkin" value="Registrarse">Registrarse</button>
   </form>
 `;
 // 
   // Evento para ingresar con cuenta de correo de google.
   
   document.getElementById('btn-checkin').addEventListener('click', () => {
-    
-  let  email = document.getElementById("email").value;
-  let  password = document.getElementById("password").value;
-  checkin();
+  
+ let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  checkin(email, password); 
   // console.log(email);
   // console.log(password);
      window.location.hash = '#/login'
