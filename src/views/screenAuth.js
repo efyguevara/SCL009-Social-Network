6ab20c9cce768bfb9cbaf112c64cffee3ad5f402
Stefany Guevara <efyguevara@gmail.com>
2019-06-05 14:21:07 -0400

    commit para mergear

--- src/views/screenAuth.js
+++ src/views/screenAuth.js
@@ -14,19 +14,19 @@ export const screenAuth = () => {
     <h2>Iniciar Sesión</h2>
     <input type="email" id="email" placeholder="&#128272; Correo" name="email" required>
     <input type="password" id="password" placeholder="&#128272; Contraseña" name="password" required>
-    <button value="Entrar">Entrar</button>
-    <button id="buttonGoogle" value="Entrar con Google">Entrar con Google</button>
-    <button id="btn-checkin" value="Registrarse">Registrarse</button>
+    <button type="button" value="Entrar">Entrar</button>
+    <button id="buttonGoogle" type="button" value="Entrar con Google">Entrar con Google</button>
+    <button id="btn-checkin" type="button" value="Registrarse">Registrarse</button>
   </form>
 `;
-// 
+
   // Evento para ingresar con cuenta de correo de google.
   
   document.getElementById('btn-checkin').addEventListener('click', () => {
     
   let  email = document.getElementById("email").value;
   let  password = document.getElementById("password").value;
-  checkin();
+  checkin(email, password);
   // console.log(email);
   // console.log(password);
      window.location.hash = '#/login'
