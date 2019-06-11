b7e53c5b25fe9ded7f90b86f6fad5d403d218452
Stefany Guevara <efyguevara@gmail.com>
2019-06-11 18:19:13 -0400

    probando ingresar solo despues de haber verificado el mail

--- src/assets/js/authFirebase.js
+++ src/assets/js/authFirebase.js
@@ -1,5 +1,6 @@
 import { notifyError } from '../js/notifications.js';
-
+// import { verifiedEmail } from '../js/validateVerifiedEmail.js'; 
+import { screenHome } from '../views/screenHome.js';
 
 export const authGoogle = () => {
   let provider = new firebase.auth.GoogleAuthProvider();
@@ -15,7 +16,6 @@ const authentication = (provider) => {
       // The signed-in user info.
       let user = result.user;
       console.log(result);
-      // ...
       saveUserInData(user);
     }).catch((error) => {
       // Handle Errors here.
@@ -66,6 +66,7 @@ export const login = (emailLogin, passwordLogin) => {
 
   firebase.auth().signInWithEmailAndPassword(emailLogin, passwordLogin)
     .then(() => {
+      observer();
       window.location.hash = '#/home';
       saveUserInData(user);
     })
@@ -76,6 +77,7 @@ export const login = (emailLogin, passwordLogin) => {
       var errorMessage = error.message;
       console.log(errorCode);
       console.log(errorMessage);
+
       notifyError(errorCode, 'error-mail');
       notifyError(errorCode, 'error-password');
     });
@@ -86,6 +88,8 @@ export const login = (emailLogin, passwordLogin) => {
 export const observer = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+
+      
       console.log("Existe usuario activo");
 
       let displayName = user.displayName;
@@ -93,19 +97,38 @@ export const observer = () => {
 
       let emailVerified = user.emailVerified;
       console.log("*****************");
-      console.log(user.emailVerified);
+      console.log(emailVerified);
       console.log("*****************");
 
       let photoUrl = user.photoURL;
       let uid = user.uid;
       let providerData = user.providerData;
       // User is signed in.
-    } else {
-      // No user is signed in.
+      login();
+      saveUserInData(user);
+    } 
+    else if (!user) {
+      verifiedEmail();
       console.log("No existe usuario activo");
     }
   });
 }
+
+
+export const verifiedEmail = (user) => {
+  let user2 = user;
+  if (user2.emailVerified) {
+    screenHome();  
+    // notifyError(errorCode, 'error-mail');
+    return true;
+  }
+  if (!user2.emailVerified){
+    console.log("Por favor verifica tu cuenta antes de ingresar");
+    notifyError(errorCode, 'error-mail');
+    window.location.hash = "#/login";
+  }
+}
+
 
 //   Guardando a mis usuarios en firestore automáticamente
 const saveUserInData = (user) => {
@@ -138,3 +161,27 @@ export const closed = () => {
     // An error happened.
   });
 }
+
+
+
+// function aparece(user) {
+//   console.log(user.email);
+//   var user = user;
+//   if (user.emailVerified) {
+//      templateMuro();
+//    return true 
+//   }
+//   if (!user.emailVerified) {
+//       console.log("el correo no ha sido verificado");
+//       swal.fire("Verificar tu correo. Cuando este ok, Inicia Sesion!");
+//       window.location.hash = "#/login";
+//     }
+// }
+
+// import { notifyError } from './notifications.js';
+
+// export const verifiedEmail = (emailVerified) => {
+//     if (emailVerified === false) {
+//         notifyError(errorCode, 'error-mail');
+//     }
+// }
