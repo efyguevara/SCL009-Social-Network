fbc85d24ebdf13149f43d55ceb10ccd7aa93496d
MarinesLemus007 <marineslemus@gmail.com>
2019-06-14 10:02:44 -0400

    se colocó input responsivo, se cambiaron clases para el css

--- src/assets/views/screenResetPassword.js
+++ src/assets/views/screenResetPassword.js
@@ -11,17 +11,17 @@ document.getElementById('root').innerHTML =
             <a href="#"> <img src="assets/img/petlogo.png" alt="Logo Pet Lovers"></a>
         </div>
         <div id="containerForm" class="container">
-            <form>
+            <form class="formfirt">
             <h2>Reestablecer contraseña</h2>
             <h4>Por favor ingrese su email para restaurar la contraseña</h4>
                 <input type="email" id="emailResetPassword" placeholder="&#9993; correo@example.com" name="email">
                 <p id="error-mail" class="err"></p>
                 
                 <!-- boton para solicitar el reestablecimiento de la contraseña -->
-                <button type="button" id="btn_reset_pass" value="Cambiar Contraseña">Cambiar Contraseña</button>
+                <button type="button" class="button" id="btn_reset_pass" value="Cambiar Contraseña">Cambiar Contraseña</button>
                 
                 <!-- Boton para volver al login -->
-                <button type="button" id="return-login">Volver</button>
+                <button type="button" class="button" id="return-login">Volver</button>
             </form>
         </div>
     </section>
@@ -32,4 +32,11 @@ document.getElementById("btn_reset_pass").addEventListener("click", () => {
     screenLogin();
     // window.location.hash = '#/login';
 })
+
+document.getElementById("return-login").addEventListener("click", () => {
+
+    screenLogin();
+     window.location.hash = '#/login';
+})
+
 }
