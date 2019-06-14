62ea6391a9c586dca8dabd248348b279f0eaa1a7
AgathaP <ebrumaktub@gmx.com>
2019-06-14 13:50:08 -0400

    arreglo en propiedades css navbar y post

--- src/assets/views/screenHome.js
+++ src/assets/views/screenHome.js
@@ -5,6 +5,7 @@ import { postUsers } from '../js/posts.js';
 export const screenHome = () => {
     document.getElementById('root').innerHTML =
         `
+<container class="screen_post">
     <header>
         <div class="container">
             <div class="row">
@@ -12,9 +13,9 @@ export const screenHome = () => {
                     <h1 class="logo">Logo <span>Empresa</span></h1>
                     <nav id="site-nav" class="site-nav">
                         <ul>
-                            <li><a href="" id="mi_perfil"><i class="fa fa-home site-nav--icon"></i>Mi perfil</a></li>
-                            <li><a href="" id="muro"><i class="fa fa-home site-nav--icon"></i>Muro</a></li>
-                            <li><a href="" id="closed"><i class="fa fa-home site-nav--icon"></i>Cerrar sesión</a></li>
+                            <li><a href="" id="mi_perfil">Mi perfil</a></li>
+                            <li><a href="" id="muro">Muro</a></li>
+                            <li><a href="" id="closed">Cerrar sesión</a></li>
                         </ul>
                     </nav>
                     <div id="menu-toggle" class="menu-toggle">
@@ -26,7 +27,7 @@ export const screenHome = () => {
 </header> 
 
     <!-- posts -->
-    <section class="section">
+    <div class="section">
     <div class="container">
         <form class="formpost">
             <div class="textform">Nombre de usuario</div>
@@ -34,8 +35,12 @@ export const screenHome = () => {
             <div class="div-button"> <button class="buttonpost" type="button"  id="sendpost">Post</button> </div>
         </form>   
     </div>
-</section>
-    `;
+
+    <!-- donde se imprimen los post -->
+    <div class="show_old_posts">
+    </div>
+</div>
+</container>`;
 
     // Cambio de clase al navbar.
     document.getElementById('menu-toggle').addEventListener('click', () => {
@@ -53,7 +58,7 @@ export const screenHome = () => {
     document.getElementById('closed').addEventListener('click', () => {
         console.log("hola");
         closed();
-        window.location.hash = '#/login'
+        window.location.hash = '#/login';
         observer();
     });
 };
